eebc0a17a87749d95951b05c27ff23298df8c977
getgou <getgou@gmail.com>
2019-10-14 15:40:17 +0200

    Initial Commit!

---
+++ src/main/java/Spring_CircleCI/Controller/HomeController.java
@@ -0,0 +1,13 @@
+package Spring_CircleCI.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String viewHome(){
+        return "index";
+    }
+}
---
+++ src/main/java/Spring_CircleCI/SpringCircleCiApplication.java
@@ -0,0 +1,13 @@
+package Spring_CircleCI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCircleCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCircleCiApplication.class, args);
+	}
+
+}
